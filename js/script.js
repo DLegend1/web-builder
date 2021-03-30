46aaa6f5a33e98f284e2fdcbf99a550f7ac35d9e
unknown <damjanmen11@gmail.com>
2021-03-30 14:34:44 +0200

    fixed a bug where it wrote in the hr element (updated an if statement)

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ function AddElement(tagname) {
     var input = document.getElementById("inline-text").value;
     //to be added more Hs
     const newElement = document.createElement(tagname);
+    debugger;
     if (input.length == 0 && tagname != "hr") {
         newElement.innerText = `${tagname} element`;
     }
-    else {
+    else if (input.length != 0 && tagname != "hr" ) {
         newElement.innerText = input;
     } 
 
